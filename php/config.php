a423dee89569452309b34b881e86b339fa31693d
czeslaw187 <czeslaw187@users.noreply.github.com>
2020-12-30 19:41:44 +0000

    database rein

--- php/config.php
+++ php/config.php
@@ -3,8 +3,8 @@
 	$cd_host = "eu-cdbr-west-03.cleardb.net";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "b673ce30edba94"; // user name
-	$cd_password = "cd9edfc4"; // password
-	$cd_dbname = "heroku_cd6b54ee44ce24a"; // database name
+	$cd_user = "b2f4a4afce60a2"; // user name
+	$cd_password = "7825b0fa"; // password
+	$cd_dbname = "heroku_8fea0586a8654ba"; // database name
 
 ?> 
