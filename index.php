6c6c90b79976ac25a0e88ad33f425b33b432c21f
czeslaw187 <czeslaw187@users.noreply.github.com>
2021-01-11 00:02:02 +0000

    5

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Company Directory</title>
+    <link rel="icon" href="images/favicon.png" type="image/x-icon"/>
     <link rel="stylesheet" href="./vendors/bootstrap-4.5.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./vendors/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
