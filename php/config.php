06a146a4798fe17af538f1b99459d05d6b859e4a
czeslaw187 <czeslaw187@users.noreply.github.com>
2021-01-16 22:10:41 +0000

    1

--- php/config.php
+++ php/config.php
@@ -3,8 +3,8 @@
 	$cd_host = "eu-cdbr-west-03.cleardb.net";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "b47f69dbec237b"; // user name
-	$cd_password = "16c7b710"; // password
-	$cd_dbname = "heroku_602c86f636840fa"; // database name
+	$cd_user = "b415ed104cddcb"; // user name
+	$cd_password = "25b4474b"; // password
+	$cd_dbname = "heroku_30542cb6f95bd84"; // database name
 
 ?>
